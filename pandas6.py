811a9ee63468c41c446d3583bc6199032cf4009b
Truong <GINA.TRUONG@VIZIENTINC.COM>
2020-08-09 07:26:48 -0500

    Groupby

--- pandas6.py
+++ pandas6.py
@@ -1,1 +1,12 @@
-# GROUPBY
+# GROUPBY - allows you to group together rows based off of a column and perform an aggregate function on them.
+import numpy as np
+import pandas as pd 
+
+data = {
+  'Company':['GOOG','GOOG','MSFT','MSFT','FB','FB'],
+  'Person':['Sam','Charlie','Amy','Vanessa','Carl','Sarah'],
+  'Sales':[200,120,340,124,243,350]
+}
+
+df=pd.DataFrame(data)
+
